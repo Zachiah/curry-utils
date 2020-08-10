d0c54d81782210616cb2fa098c9029fc76def1f2
Zachiah Sawyer <zachiahsawyer1@gmail.com>
2020-08-10 16:07:05 -0600

    add mult and div

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 exports.add = (a) => (b) => (b + a);
 exports.sub = (a) => (b) => (b - a);
+exports.mult = (a) => (b) => (b * a);
+exports.div = (a) => (b) => (b / a);
--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const {add,sub} = require('../index.js');
+const {add,sub,mult,div} = require('../index.js');
 const assert = require('assert');
 
 describe("add", function() {
@@ -13,4 +13,18 @@ describe("sub", function() {
         assert.strictEqual(sub(2)(3), 1);
         assert.deepStrictEqual([4,5,6].map(sub(2)), [2,3,4])
     })
+});
+
+describe("mult", function() {
+    it("works", function() {
+        assert.strictEqual(mult(2)(3),6);
+        assert.deepStrictEqual([1,2,3].map(mult(2)), [2,4,6]);
+    });
+});
+
+describe("div", function() {
+    it("works", function() {
+        assert.strictEqual(div(2)(6),3);
+        assert.deepStrictEqual([2,4,6].map(div(2)), [1,2,3]);
+    });
 });
